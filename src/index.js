8d8cc6add81896dad62e3f0081398cb887e74da3
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2022-10-03 23:44:12 +0800

    add store

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Connect, Provider } from "react-redux";
+import store from './reduxStore'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
